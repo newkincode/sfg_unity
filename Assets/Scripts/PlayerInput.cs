b2fc2f7b816d422d8cdab55ab2c6614c4ae7a365
newkincode <maainnewkin59@gmail.com>
2023-03-25 22:13:04 +0900

    에러해결? 인줄

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMove : MonoBehaviour
+public class playerInput : MonoBehaviour
 {
     // move
     void Move()
