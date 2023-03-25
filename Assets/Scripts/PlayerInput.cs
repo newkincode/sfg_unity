5e6ba0fe971d2c212378f4284caa9e78b36af194
newkincode <maainnewkin59@gmail.com>
2023-03-25 23:08:47 +0900

    클릭이벤트

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerInput : MonoBehaviour
+public class PlayerInput : MonoBehaviour
 {
     // move
     void Move()
